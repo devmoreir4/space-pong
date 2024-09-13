225f498b67b5802bb731f6c96769117543a94db3
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-09-13 17:28:16 -0300

    feat: add collisions with asteroids

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,13 +4,23 @@ extends Node2D
 @onready var label_pongs = $Pongs
 @onready var label_tutorial = $Tutorial
 @onready var positions = $Positions
+@onready var background = $Background
+
 var last_position
 var asteroid_scene = preload("res://scenes/asteroid.tscn")
+
+var resource_asteroids = {}
+var resource_background = {}
+var resource_colors_label = {}
+
+func _ready():
+	preload_resources()
 
 func _process(delta):
 	if ball.started:
 		label_pongs.visible = true
 		label_tutorial.visible = false
+		check_pongs(ball.pongs)
 	
 	label_pongs.text = str(ball.pongs)
 
@@ -36,3 +46,61 @@ func spawn_asteroid():
 			last_position = spawn_position
 		else:
 			spawn_asteroid()
+
+func preload_resources():
+	resource_asteroids = {
+		"asteroid1" : preload("res://assets/Asteroide1.png"),
+		"asteroid2" : preload("res://assets/Asteroide2.png"),
+		"asteroid3" : preload("res://assets/Asteroide3.png"),
+		"asteroid4" : preload("res://assets/Asteroide4.png"),
+		"asteroid5" : preload("res://assets/Asteroide5.png"),
+		"asteroid6" : preload("res://assets/Asteroide6.png")
+	}
+	
+	resource_background = {
+		"background1" : preload("res://assets/Fundo1.png"),
+		"background2" : preload("res://assets/Fundo2.png"),
+		"background3" : preload("res://assets/Fundo3.png"),
+		"background4" : preload("res://assets/Fundo4.png"),
+		"background5" : preload("res://assets/Fundo5.png"),
+		"background6" : preload("res://assets/Fundo6.png")
+	}
+	
+	resource_colors_label = {
+		"color1" : "7301ec",
+		"color2" : "4196ff",
+		"color3" : "4ea771",
+		"color4" : "fe9c35",
+		"color5" : "ff5d5d",
+		"color6" : "762d79"
+	}
+
+func check_pongs(pongs):
+	match pongs:
+		0:
+			update_colors("color1", "background1")
+		10:
+			update_colors("color2", "background2")
+			update_asteroids("asteroid2")
+		20:
+			update_colors("color3", "background3")
+			update_asteroids("asteroid3")
+		30:
+			update_colors("color4", "background4")
+			update_asteroids("asteroid4")
+		40:
+			update_colors("color5", "background5")
+			update_asteroids("asteroid5")
+		50:
+			update_colors("color6", "background6")
+			update_asteroids("asteroid6")
+
+func update_colors(key_color_label, key_color_bg):
+	label_pongs.label_settings.font_color = resource_colors_label[key_color_label]
+	background.set_texture(resource_background[key_color_bg])
+	
+func update_asteroids(key_asteroid):
+	var asteroids = get_tree().get_nodes_in_group("asteroids")
+	
+	for asteroid in asteroids:
+		asteroid.get_node("Sprite2D").texture = resource_asteroids[key_asteroid]
